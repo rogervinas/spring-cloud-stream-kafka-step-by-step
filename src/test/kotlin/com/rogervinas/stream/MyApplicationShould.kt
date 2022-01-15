7bb87dd617d6acbb2f38028f53ba093c2825c642
Roger Viñas <roger.vinas@scmspain.com>
2022-01-15 19:09:47 +0100

    Fix compilation errors

--- src/test/kotlin/com/rogervinas/stream/MyApplicationShould.kt
+++ src/test/kotlin/com/rogervinas/stream/MyApplicationShould.kt
@@ -19,113 +19,99 @@ import org.springframework.core.env.Environment
 import org.springframework.test.context.ActiveProfiles
 import java.time.Duration
 import java.util.*
+import java.util.function.Consumer
 
 @SpringBootTest(webEnvironment = DEFINED_PORT)
 @ActiveProfiles("docker-compose")
 class MyApplicationShould {
 
-    val TOPIC = "my.topic"
-    val TOPIC_DLQ = "my.topic.errors"
+  val TOPIC = "my.topic"
+  val TOPIC_DLQ = "my.topic.errors"
 
-    val TEN_SECONDS = Duration.ofSeconds(10)
-    val FIVE = 5
+  val TEN_SECONDS = Duration.ofSeconds(10)
+  val FIVE = 5
 
-    @Autowired
-    lateinit var env: Environment
+  @Autowired
+  lateinit var env: Environment
 
-    @Autowired
-    lateinit var eventProducer: MyEventProducer
+  @Autowired
+  lateinit var eventProducer: MyEventProducer
 
-    @MockBean
-    lateinit var eventConsumer: MyEventConsumer
+  @MockBean
+  lateinit var eventConsumer: MyEventConsumer
 
-    lateinit var kafkaProducerHelper: MyKafkaProducerHelper
-    lateinit var kafkaConsumerHelper: MyKafkaConsumerHelper
-    lateinit var kafkaDLQConsumerHelper: MyKafkaConsumerHelper
+  lateinit var kafkaProducerHelper: MyKafkaProducerHelper
+  lateinit var kafkaConsumerHelper: MyKafkaConsumerHelper
+  lateinit var kafkaDLQConsumerHelper: MyKafkaConsumerHelper
 
-    @BeforeEach
-    fun setUp() {
-        val bootstrapServers = env.getProperty("spring.cloud.stream.kafka.binder.brokers")!!
-        kafkaConsumerHelper = MyKafkaConsumerHelper(bootstrapServers, TOPIC)
-        kafkaConsumerHelper.consumeAll()
-        kafkaDLQConsumerHelper = MyKafkaConsumerHelper(bootstrapServers, TOPIC_DLQ)
-        kafkaDLQConsumerHelper.consumeAll()
-        kafkaProducerHelper = MyKafkaProducerHelper(bootstrapServers)
-    }
+  @BeforeEach
+  fun setUp() {
+    val bootstrapServers = env.getProperty("spring.cloud.stream.kafka.binder.brokers")!!
+    kafkaConsumerHelper = MyKafkaConsumerHelper(bootstrapServers, TOPIC)
+    kafkaConsumerHelper.consumeAll()
+    kafkaDLQConsumerHelper = MyKafkaConsumerHelper(bootstrapServers, TOPIC_DLQ)
+    kafkaDLQConsumerHelper.consumeAll()
+    kafkaProducerHelper = MyKafkaProducerHelper(bootstrapServers)
+  }
 
-    @Test
-    fun `produce event`() {
-        val text = "hello ${UUID.randomUUID()}"
-        eventProducer.produce(MyEvent(text))
+  @Test
+  fun `produce event`() {
+    val text = "hello ${UUID.randomUUID()}"
+    eventProducer.produce(MyEvent(text))
 
-        val records = kafkaConsumerHelper.consumeAtLeast(1, TEN_SECONDS)
+    val records = kafkaConsumerHelper.consumeAtLeast(1, TEN_SECONDS)
 
-        assertThat(records)
-                .singleElement().satisfies { record ->
-                    JSONAssert.assertEquals(
-                            record.value(),
-                            "{\"number\":${text.length},\"string\":\"$text\"}",
-                            true
-                    )
-                    assertThat(record.key())
-                            .isEqualTo("key-${text.length}")
-                }
-    }
+    assertThat(records).singleElement().satisfies(Consumer { record ->
+      JSONAssert.assertEquals(record.value(), "{\"number\":${text.length},\"string\":\"$text\"}", true)
+      assertThat(record.key()).isEqualTo("key-${text.length}")
+    })
+  }
 
-    @Test
-    fun `consume event`() {
-        val text = "hello ${UUID.randomUUID()}"
-        kafkaProducerHelper.send(TOPIC, "{\"number\":${text.length},\"string\":\"$text\"}")
+  @Test
+  fun `consume event`() {
+    val text = "hello ${UUID.randomUUID()}"
+    kafkaProducerHelper.send(TOPIC, "{\"number\":${text.length},\"string\":\"$text\"}")
 
-        val eventCaptor = argumentCaptor<MyEvent>()
-        verify(eventConsumer, timeout(TEN_SECONDS.toMillis())).consume(eventCaptor.capture())
+    val eventCaptor = argumentCaptor<MyEvent>()
+    verify(eventConsumer, timeout(TEN_SECONDS.toMillis())).consume(eventCaptor.capture())
 
-        assertThat(eventCaptor.firstValue).satisfies { event -> assertThat(event.text).isEqualTo(text) }
-    }
+    assertThat(eventCaptor.firstValue).satisfies(Consumer { event -> assertThat(event.text).isEqualTo(text) })
+  }
 
-    @Test
-    fun `retry consume event 5 times`() {
-        doThrow(MyRetryableException("retry later!")).`when`(eventConsumer).consume(any())
+  @Test
+  fun `retry consume event 5 times`() {
+    doThrow(MyRetryableException("retry later!")).`when`(eventConsumer).consume(any())
 
-        val text = "hello ${UUID.randomUUID()}"
-        kafkaProducerHelper.send(TOPIC, "{\"number\":${text.length},\"string\":\"$text\"}")
+    val text = "hello ${UUID.randomUUID()}"
+    kafkaProducerHelper.send(TOPIC, "{\"number\":${text.length},\"string\":\"$text\"}")
 
-        val eventCaptor = argumentCaptor<MyEvent>()
-        verify(eventConsumer, timeout(TEN_SECONDS.toMillis()).times(FIVE)).consume(eventCaptor.capture())
+    val eventCaptor = argumentCaptor<MyEvent>()
+    verify(eventConsumer, timeout(TEN_SECONDS.toMillis()).times(FIVE)).consume(eventCaptor.capture())
 
-        assertThat(eventCaptor.allValues).allSatisfy { event -> assertThat(event.text).isEqualTo(text) }
-    }
+    assertThat(eventCaptor.allValues).allSatisfy(Consumer { event -> assertThat(event.text).isEqualTo(text) })
+  }
 
-    @Test
-    fun `send to DLQ rejected messages`() {
-        doThrow(MyRetryableException("retry later!")).`when`(eventConsumer).consume(any())
+  @Test
+  fun `send to DLQ rejected messages`() {
+    doThrow(MyRetryableException("retry later!")).`when`(eventConsumer).consume(any())
 
-        val text = "hello ${UUID.randomUUID()}"
-        kafkaProducerHelper.send(TOPIC, "{\"number\":${text.length},\"string\":\"$text\"}")
+    val text = "hello ${UUID.randomUUID()}"
+    kafkaProducerHelper.send(TOPIC, "{\"number\":${text.length},\"string\":\"$text\"}")
 
-        val errorRecords = kafkaDLQConsumerHelper.consumeAtLeast(1, TEN_SECONDS)
-        assertThat(errorRecords)
-                .singleElement().satisfies { record ->
-                    JSONAssert.assertEquals(
-                            record.value(),
-                            "{\"number\":${text.length},\"string\":\"$text\"}",
-                            true
-                    )
-                }
-    }
+    val errorRecords = kafkaDLQConsumerHelper.consumeAtLeast(1, TEN_SECONDS)
+    assertThat(errorRecords).singleElement().satisfies(Consumer { record ->
+      JSONAssert.assertEquals(record.value(), "{\"number\":${text.length},\"string\":\"$text\"}", true)
+    })
+  }
 
-    @ParameterizedTest
-    @ValueSource(strings = [
-        "plain text",
-        "{\"unknownField\":\"not expected\"}"
-    ])
-    fun `send to DLQ undeserializable messages`(body: String) {
-        kafkaProducerHelper.send(TOPIC, body)
+  @ParameterizedTest
+  @ValueSource(strings = ["plain text", "{\"unknownField\":\"not expected\"}"])
+  fun `send to DLQ undeserializable messages`(body: String) {
+    kafkaProducerHelper.send(TOPIC, body)
 
-        val errorRecords = kafkaDLQConsumerHelper.consumeAtLeast(1, TEN_SECONDS)
-        assertThat(errorRecords)
-                .singleElement().satisfies { record ->
-                    assertThat(record.value()).isEqualTo(body)
-                }
-    }
+    val errorRecords = kafkaDLQConsumerHelper.consumeAtLeast(1, TEN_SECONDS)
+    assertThat(errorRecords).singleElement().satisfies(Consumer { record ->
+      assertThat(record.value()).isEqualTo(body)
+    })
+  }
 }
