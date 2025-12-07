623e2fa7e468df73a4d81e32e9fac62ad53a67b2
Roger Viñas Alcon <roger.vinas@fastzink.com>
2025-12-07 19:03:28 +0100

    Add Jackson and Kotlin module dependencies (#103)

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,9 @@ val testContainersVersion = "1.21.3"
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.cloud:spring-cloud-starter-stream-kafka")
+
   implementation("org.springframework.boot:spring-boot-starter-jackson")
+  implementation("tools.jackson.module:jackson-module-kotlin")
 
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
